784d026a1726a2f0d6c9bbc4ceb6ad5da7cba2b2
JaeChang <wockd9600@naver.com>
2023-09-08 19:11:28 +0900

    get raffle result from promise

--- Raffle/js/util.js
+++ Raffle/js/util.js
@@ -1,53 +1,60 @@
-const startRaffle = (data) => {
-    const { arr, box, result_number, duplicate, length, viewNow } = data;
-    const checked = new Array(length).fill(false);
-    const shuffleIndex = [...Array(length).keys()].map(x => x + 1);
-    const count = new Array(length).fill(0);
+function startRaffle(data) {
+    return new Promise((resolve, reject) => {
 
-    for (let i = length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffleIndex[i], shuffleIndex[j]] = [shuffleIndex[j], shuffleIndex[i]];
-    }
+        const { arr, box, result_number, duplicate, length, viewNow } = data;
+        // const checked = new Array(length).fill(false);
+        const shuffleIndex = [...Array(length).keys()].map(x => x + 1);
+        const count = new Array(length).fill(0);
+
+        for (let i = length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffleIndex[i], shuffleIndex[j]] = [shuffleIndex[j], shuffleIndex[i]];
+        }
 
 
-    for (let i = 0; i < result_number; i++) {
-        const span = document.createElement('span');
-        span.classList.add('span')
-        span.innerText = '';
-        box.appendChild(span);
+        const j = viewNow ? 30 : 700;
 
-        (function (i) {
-            
-            setTimeout(() => {
-                const span = document.getElementsByClassName('span')[i];
+        for (let i = 0; i < result_number; i++) {
+            const span = document.createElement('span');
+            span.classList.add('span')
+            span.innerText = '';
+            box.appendChild(span);
+
+            (function (i) {
                 
-                let speed = 40;
-                
-                let index = shuffleIndex[i];
-                
-                const interval = setInterval(() => {
-                    do {
-                        index = index < length - 1 ? index + 1 : 0;
-                        span.innerText = arr[index];
-                    } while (checked[index]);
-                }, speed);
-                
-
-                const j = viewNow ? 20 : 700;
-
                 setTimeout(() => {
-                    if (duplicate) checked[index] = true;
-                    count[index] += 1
-                    clearInterval(interval);
+                    const span = document.getElementsByClassName('span')[i];
+                    
+                    let speed = 40;
+                    
+                    let index = shuffleIndex[i];
+                    
+                    const interval = setInterval(() => {
+                        do {
+                            index = index < length - 1 ? index + 1 : 0;
+                            span.innerText = arr[index];
+                        } while (count[index] > 0 && duplicate);
+                    }, speed);
+                    
 
-                    operateButton();
-                }, 4000 + i * j);
-            }, 100);
-        })(i);
-    }
-    setTimeout(() => {
-        console.log(count)
-    }, 5000)
+
+                    setTimeout(() => {
+                        count[index] += 1;
+                        // count[index] += 1
+                        clearInterval(interval);
+
+                    }, 4000 + i * j);
+                }, 100);
+            })(i);
+        }
+
+        const k = viewNow ? 500 : 0;
+        setTimeout(() => {
+            operateButton();
+            resolve({arr, count});
+            // console.log(count)
+        }, 4000 + result_number * j + k);
+    });
 }
 
 export {
